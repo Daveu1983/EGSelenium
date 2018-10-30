51976624e96d3b7bdb310e0bbdf52ac82d1870d9
Daveu1983 <31164433+Daveu1983@users.noreply.github.com>
2018-10-30 19:47:05 +0000

    Extracted local variable links to field

--- src/test/java/EGTest.java
+++ src/test/java/EGTest.java
@@ -6,20 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class EGTest {
+    private List<String> links = Arrays.asList("Schedule","Tickets","Sponsors",
+            "Code of conduct","Contact","About");
     @Test
     public void testNavigateJmanc() throws InterruptedException{
         WebDriver driver = new FirefoxDriver();
         driver.get("https://mcrjava.github.io/jmanc/");
-        WebElement link;
-        final ArrayList<String> links = new ArrayList<String>();
-        links.add("Schedule");
-        links.add("Tickets");
-        links.add("Sponsors");
-        links.add("Code of conduct");
-        links.add("Contact");
-        links.add("About");
         checkTabs(driver, links);
         driver.quit();
         driver = new ChromeDriver();
@@ -29,7 +25,7 @@ public class EGTest {
 
     }
 
-    private void checkTabs(WebDriver driver, ArrayList<String> links) throws InterruptedException {
+    private void checkTabs(WebDriver driver, List<String> links) throws InterruptedException {
         WebElement link;
         for(String item:links) {
             link = driver.findElement(By.linkText(item));
